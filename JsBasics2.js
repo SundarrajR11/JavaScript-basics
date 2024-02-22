ba26f74631d4688e5b3b4bf854c0cb69a3dbadfe
Sundarraj R <ragusundar274@gmail.com>
2024-02-22 21:43:14 +0530

    JsBasics covered on 22022024-21:42

---
+++ JsBasics2.js
@@ -0,0 +1,19 @@
+// No impact on const keyword when it use ! 
+const flag =false
+if(!flag){
+    console.log("Const keyword works for !")
+}else{
+    console.log("Const keywors not works for !")
+}
+// print numbers from 1 to 10 using while loop
+let i=1
+while(i<=10){
+    console.log(i)
+    i++
+}
+// print numbers from 1 to 10 using do while loop
+let j=4;
+do{
+    console.log(j)
+    j++
+}while(j<10);
